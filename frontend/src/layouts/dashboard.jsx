33923b9fc0af426b68013725af76a829c1ed4e71
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-16 15:09:44 -0500

    Removing Configurator

--- frontend/src/layouts/dashboard.jsx
+++ frontend/src/layouts/dashboard.jsx
@@ -4,7 +4,6 @@ import { IconButton } from "@material-tailwind/react";
 import {
   Sidenav,
   DashboardNavbar,
-  Configurator,
   Footer,
 } from "@/widgets/layout";
 import routes from "@/routes";
@@ -24,16 +23,6 @@ export function Dashboard() {
       />
       <div className="p-4 xl:ml-80">
         <DashboardNavbar />
-        <Configurator />
-        <IconButton
-          size="lg"
-          color="white"
-          className="fixed bottom-8 right-8 z-40 rounded-full shadow-blue-gray-900/10"
-          ripple={false}
-          onClick={() => setOpenConfigurator(dispatch, true)}
-        >
-          <Cog6ToothIcon className="h-5 w-5" />
-        </IconButton>
         <Routes>
           {routes.map(
             ({ layout, pages }) =>
