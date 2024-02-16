2c090e96df47f2e84804c1a466cfd3a7e6138c13
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-16 15:33:56 -0500

    Removing Authors Data File

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -5,4 +5,3 @@ export * from "@/data/orders-overview-data";
 export * from "@/data/platform-settings-data";
 export * from "@/data/conversations-data";
 export * from "@/data/projects-data";
-export * from "@/data/authors-table-data";
