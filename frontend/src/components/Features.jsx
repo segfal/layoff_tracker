7b5e40258ac9b6cd5da8328be1186430b3f1bb4f
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-15 20:46:22 -0500

    set up react-router-dom

---
+++ frontend/src/components/Features.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import right_arrow from "../images/right_arrow.png";
+export default function FeatureComponent({logo, title, description1, description2}) {
+  return (
+    <></>
+  )
+}
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function LandingPage(){
+    return (
+        <></>
+    )
+}
+export default LandingPage
