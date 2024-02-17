b19913198d9fe97c74a8c108eca4cbd254d65110
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-16 19:48:52 -0500

    Removing Data Files

--- frontend/src/pages/dashboard/home.jsx
+++ frontend/src/pages/dashboard/home.jsx
@@ -22,9 +22,8 @@ import { StatisticsChart } from "@/widgets/charts";
 import {
   statisticsCardsData,
   statisticsChartsData,
-  projectsTableData,
-  ordersOverviewData,
 } from "@/data";
+
 import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 
 export function Home() {
