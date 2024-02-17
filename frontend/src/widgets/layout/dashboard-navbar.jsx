0ae5ae0c54047d0b3028498b7d010ff7cd8ea8ea
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-16 20:04:30 -0500

    Removes State Management

--- frontend/src/widgets/layout/dashboard-navbar.jsx
+++ frontend/src/widgets/layout/dashboard-navbar.jsx
@@ -20,34 +20,25 @@ import {
   CreditCardIcon,
   Bars3Icon,
 } from "@heroicons/react/24/solid";
-import {
-  useMaterialTailwindController,
-  setOpenConfigurator,
-  setOpenSidenav,
-} from "@/context";
+
 
 export function DashboardNavbar() {
-  const [controller, dispatch] = useMaterialTailwindController();
-  const { fixedNavbar, openSidenav } = controller;
   const { pathname } = useLocation();
   const [layout, page] = pathname.split("/").filter((el) => el !== "");
 
   return (
     <Navbar
-      color={fixedNavbar ? "white" : "transparent"}
+      color={ "white"}
       className={`rounded-xl transition-all ${
-        fixedNavbar
-          ? "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
-          : "px-0 py-1"
+          "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
       }`}
       fullWidth
-      blurred={fixedNavbar}
     >
       <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
         <div className="capitalize">
           <Breadcrumbs
             className={`bg-transparent p-0 transition-all ${
-              fixedNavbar ? "mt-1" : ""
+              "mt-1"
             }`}
           >
             <Link to={`/${layout}`}>
@@ -79,7 +70,6 @@ export function DashboardNavbar() {
             variant="text"
             color="blue-gray"
             className="grid xl:hidden"
-            onClick={() => setOpenSidenav(dispatch, !openSidenav)}
           >
             <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
           </IconButton>
