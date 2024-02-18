cf57ef39c1c9996b803b4e1dcf2cb85b9ff1c94f
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-18 17:02:46 -0500

    fix pagintion issue

--- frontend/src/pages/NewsPage.jsx
+++ frontend/src/pages/NewsPage.jsx
@@ -1,43 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import Grid from '@mui/material/Grid';
-// import MediaCard from '../components/MediaCard';
-
-// const NewsPage = () => {
-//   const [articles, setArticles] = useState([]);
-
-//   useEffect(() => {
-//     const apiKey = import.meta.env.VITE_API_KEY;
-//     const baseUrl = "https://newsapi.org/v2/everything";
-//     const query = "Company layoff";
-
-//     console.log('API Key:', apiKey); // Debugging line
-
-//     axios.get(`${baseUrl}?q=${encodeURIComponent(query)}&apiKey=${apiKey}`)
-//       .then(response => {
-//         console.log('Data fetched:', response.data); // Debugging line
-//         setArticles(response.data.articles);
-//       })
-//       .catch(err => {
-//         console.error('Error fetching data:', err); // Debugging line
-//       });
-//   }, []);
-
-
-//   return (
-//     <Grid container spacing={12}>
-//       {articles.map((article, index) => (
-//         <Grid item key={index} xs={12} sm={6} md={4}>
-//           <MediaCard article={article} />
-//         </Grid>
-//       ))}
-//     </Grid>
-//   );
-// };
-
-// export default NewsPage;
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -103,8 +64,6 @@ const NewsPage = () => {
     },
   };
 
-
-
   return (
     <div>
       {/* Layoffs Insight App Info */}
@@ -138,4 +97,4 @@ const NewsPage = () => {
   );
 };
 
-export default NewsPage;
+export default NewsPage
