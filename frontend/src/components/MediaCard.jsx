f947bea382aa026e41a03876fa73e5976ccb68f7
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-18 16:06:15 -0500

    add author and publisher name inside media card

--- frontend/src/components/MediaCard.jsx
+++ frontend/src/components/MediaCard.jsx
@@ -1,43 +1,70 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function MediaCard({ article }) {
   return (
-    <Card sx={{ 
-      display: 'flex',
-      flexDirection: 'column',
-      maxWidth: 345,
-      minHeight: 400 // Use minHeight to ensure all cards have the same starting height
-    }}>
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: 345,
+        minHeight: 300, // Use minHeight to ensure all cards have the same starting height
+      }}
+    >
       <CardMedia
         component="img"
         sx={{ height: 140 }} // Fixed height for the image
         image={article.urlToImage}
         alt={article.title}
       />
-      <CardContent sx={{ flexGrow: 1, overflow: 'hidden' }}>
-        <Typography gutterBottom variant="h5" component="div" sx={{ 
-          whiteSpace: 'nowrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis'
-        }}>
+      <CardContent sx={{ flexGrow: 1, overflow: "hidden" }}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
           {article.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ 
-          display: '-webkit-box',
-          overflow: 'hidden',
-          WebkitBoxOrient: 'vertical',
-          WebkitLineClamp: 3 // Limit to 3 lines of text
-        }}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            display: "-webkit-box",
+            overflow: "hidden",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 3, // Limit to 3 lines of text
+            mb: 2
+          }}
+        >
           {article.description}
         </Typography>
+          <></>
+        <Typography
+          gutterBottom
+          variant="body"
+          component="div"
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            mt:2,
+            color: '#757575'
+          }}
+        >
+          {article.author}, {article.source.name}
+        </Typography>
       </CardContent>
-      <CardActions sx={{ mt: 'auto' }}>
+      <CardActions sx={{ mt: "auto" }}>
         <Button size="small" color="primary" href={article.url} target="_blank">
           Learn More
         </Button>
