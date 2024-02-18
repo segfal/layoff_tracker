f7eb1cd7bdb069a64b447d9d08d96ad8ba81374e
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-17 21:20:39 -0500

    Fixes per company Component

--- frontend/src/pages/dashboard/home.jsx
+++ frontend/src/pages/dashboard/home.jsx
@@ -63,7 +63,7 @@ export function Home() {
       </div>
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
         <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
-          <CardHeader
+          {/* <CardHeader
             floated={false}
             shadow={false}
             color="transparent"
@@ -77,11 +77,11 @@ export function Home() {
                 variant="small"
                 className="flex items-center gap-1 font-normal text-blue-gray-600"
               >
-                {/* <CheckCircleIcon strokeWidth={3} className="h-4 w-4 text-blue-gray-200" /> */}
-                {/* <strong>30 done</strong> this month */}
+                <CheckCircleIcon strokeWidth={3} className="h-4 w-4 text-blue-gray-200" />
+                <strong>30 done</strong> this month
               </Typography>
             </div>
-          </CardHeader>
+          </CardHeader> */}
             <StatisticsChart
               key={statisticsChartsData[3].title}
               {...statisticsChartsData[3]}
