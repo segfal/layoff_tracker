6784e2c8b8c4acb76157453c6eb860d1e17e3523
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-18 22:37:29 -0500

    add typing effect component

---
+++ frontend/src/components/TypingEffect.jsx
@@ -0,0 +1,26 @@
+import Typed from 'typed.js'
+import React, {useEffect, useState} from 'react'
+
+const TypingEffect = ({ strings }) => {
+    const el = useRef(null);
+    const typed = useRef(null);
+  
+    useEffect(() => {
+      const options = {
+        strings,
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: false,
+      };
+  
+      typed.current = new Typed(el.current, options);
+  
+      return () => {
+        typed.current.destroy();
+      };
+    }, [strings]);
+  
+    return <span ref={el} />;
+  };
+  
+  export default TypingEffect;
