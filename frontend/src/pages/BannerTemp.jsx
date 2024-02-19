f26f3680ec6bd9b4a172d109bf1d8785e8ae114d
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-19 17:55:45 -0500

    add Banner to newspage

--- frontend/src/pages/BannerTemp.jsx
+++ frontend/src/pages/BannerTemp.jsx
@@ -1,39 +1,66 @@
-import img from '../images/fired.jpeg'
-
+import img from "../images/fired.jpeg";
+import news from "../images/news.jpeg";
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row md:space-x-2 px-2 lg:p-0">
-    <a 
-      className="mb-4 md:mb-0 w-full md:w-2/3 relative rounded inline-block" 
-      style={{ height: '24em' }}
-      href="#"
-    >
-      <div className="absolute left-0 bottom-0 w-full h-full z-10"
-        style={{ backgroundImage: 'linear-gradient(180deg,transparent,rgba(0,0,0,.7))' }}></div>
-      <img src={img} className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="First Example" />
-      <div className="p-4 absolute bottom-0 left-0 z-20">
-        <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Layoffs</span>
-        <h2 className="md:text-4xl text-3xl font-semibold text-gray-100 leading-tight">
-          Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
-        </h2>
-        
-      </div>
-    </a>
+      <a
+        className="mb-4 md:mb-0 w-full md:w-2/3 relative rounded inline-block"
+        style={{ height: "24em" }}
+        href="#"
+      >
+        <div
+          className="absolute left-0 bottom-0 w-full h-full z-10"
+          style={{
+            backgroundImage:
+              "linear-gradient(180deg,transparent,rgba(0,0,0,.7))",
+          }}
+        ></div>
+        <img
+          src={img}
+          className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover"
+          alt="First Example"
+        />
+        <div className="p-4 absolute bottom-0 left-0 z-20">
+          <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">
+            Layoffs
+          </span>
+          <h2 className="md:text-4xl text-3xl font-semibold text-gray-100 leading-tight">
+            The tech industry is facing a wave of layoffs as companies
+            reevaluate their growth and scalability strategies amidst economic
+            shifts.
+          </h2>
+        </div>
+      </a>
 
-    <a className="w-full md:w-1/3 relative rounded" 
-      style={{ height: '24em' }}
-      href="#"
-    >
-      <div className="absolute left-0 top-0 w-full h-full z-10"
-        style={{ backgroundImage: 'linear-gradient(180deg,transparent,rgba(0,0,0,.7))' }}></div>
-      <img src="https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80" className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="Second Example" />
-      <div className="p-4 absolute bottom-0 left-0 z-20">
-        <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Science</span>
-        <h2 className="text-3xl font-semibold text-gray-100 leading-tight">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
-      </div>
-    </a>
-  </div>
-  )
-}
+      <a
+        className="w-full md:w-1/3 relative rounded"
+        style={{ height: "24em" }}
+        href="#"
+      >
+        <div
+          className="absolute left-0 top-0 w-full h-full z-10"
+          style={{
+            backgroundImage:
+              "linear-gradient(180deg,transparent,rgba(0,0,0,.7))",
+          }}
+        ></div>
+        <img
+          src={news}
+          className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover"
+          alt="Second Example"
+        />
+        <div className="p-4 absolute bottom-0 left-0 z-20">
+          <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">
+            News
+          </span>
+          <h2 className="text-3xl font-semibold text-gray-100 leading-tight">
+            {" "}
+            Layoff Insights is your trusted source for layoff news and insights.
+          </h2>
+        </div>
+      </a>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import NewsPage from "./NewsPages";
 import Banner from "./BannerTemp";
-import Subscribe from "./Subscribe";
+import SideBarDivider from "./SideBarDivider";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    
     <div className="">
-        <div>
-            <Navbar/>
-        </div>
+      <div>
+        <Navbar />
+      </div>
       <Banner />
 
       <div className="block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10">
         {/* post cards */}
         <NewsPage />
 
-        {/* <Subscribe/> */}
-        <Subscribe />
+        <SideBarDivider />
       </div>
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
