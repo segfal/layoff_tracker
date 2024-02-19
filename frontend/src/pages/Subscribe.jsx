3e41360176d6127679e17fd1c2d55b019b27dab4
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-02-19 18:07:04 -0500

    spell fix in susbcribe

--- frontend/src/pages/Subscribe.jsx
+++ frontend/src/pages/Subscribe.jsx
@@ -5,7 +5,7 @@ const Subscribe = () => {
     <div className="p-1 mt-4 mb-4">
       <h5 className="font-bold text-lg uppercase text-gray-700 mb-2"> Subscribe </h5>
       <p className="text-gray-600">
-        Subscribe to our newsletter. We deliver the best health-related articles to your inbox
+        Subscribe to our newsletter. We deliver the best tech-related articles to your inbox
       </p>
       <input placeholder="your email address"
         className="text-gray-700 bg-gray-100 rounded-t hover:outline-none p-2 w-full mt-4 border" />
