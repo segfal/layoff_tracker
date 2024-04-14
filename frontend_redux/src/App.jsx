7ca1754cc77fa06cf13a5f4e7d0ab167a70fca2e
segfal <segfaultboi@gmail.com>
2024-04-13 23:48:12 -0400

    Added Landing Page

--- frontend_redux/src/App.jsx
+++ frontend_redux/src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react";
-
+import LandingPage from "./Components/LandingPage";
 
 const App = () => {
   return(
-
-    <div className="app">
-      <div className="section parallax bg1">
-        <h1>Hello World</h1>
-        <button>Click me</button>
-      </div>
-      <div className="section static">
-        <h1>Static Section</h1>
-        </div>
-      <div className="section parallax bg2">
-        <h1>Parallax Section</h1>
-        </div>
+    <div>
+      <LandingPage />
     </div>
+
   )
 }
 
---
+++ frontend_redux/src/Components/LandingPage.jsx
@@ -0,0 +1,33 @@
+
+
+
+import './LandingPage.css'
+
+
+const LandingPage = () => {
+    return(
+            
+        <div className="app">
+        <div className="section parallax bg1">
+        <h1>Hello World</h1>
+        <button>Click me</button>
+        </div>
+        <div className="section static">
+        <h1>Static Section</h1>
+        </div>
+        <div className="section parallax bg2">
+        <h1>Parallax Section</h1>
+        </div>
+        <div className="section static">
+            <h1>Static Section</h1>
+        </div>
+        <div className="section parallax bg3">
+            <h1>Parallax Section</h1>
+        </div>
+        
+    </div>
+    )
+}
+
+
+export default LandingPage;
