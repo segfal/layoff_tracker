e469d6e7cb8568bbafc04834ba963ac76997e527
segfal <segfaultboi@gmail.com>
2024-04-13 23:55:01 -0400

    fixed formatting for landing page.jsx

--- frontend_redux/src/Components/LandingPage.jsx
+++ frontend_redux/src/Components/LandingPage.jsx
@@ -1,6 +1,3 @@
-
-
-
 import './LandingPage.css'
 
 
@@ -8,21 +5,26 @@ const LandingPage = () => {
     return(
             
         <div className="app">
-        <div className="section parallax bg1">
-        <h1>Hello World</h1>
-        <button>Click me</button>
-        </div>
+            <div className="section parallax bg1">
+                <div className='container'>
+                    <h1>Hello World</h1>
+                    <button>Click me</button>
+                </div>
+            </div>
         <div className="section static">
-        <h1>Static Section</h1>
+            <h1>Static Section</h1>
         </div>
         <div className="section parallax bg2">
-        <h1>Parallax Section</h1>
+            <h1>Parallax Section</h1>
         </div>
         <div className="section static">
             <h1>Static Section</h1>
         </div>
         <div className="section parallax bg3">
-            <h1>Parallax Section</h1>
+            <div className='container'>
+                <h1>Parallax Section</h1>
+                <button>Click here to find out</button>
+            </div>
         </div>
         
     </div>
