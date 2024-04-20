300ef1eef61b548b30c5f2c1111321042814d8b5
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-19 23:56:59 -0400

    create a filter by state, pagination is apply to filters, route create it to return all state name + count

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const {
-  getSpreadSheet,
+  getAllLayoff,
   getTotalRecordCountSpreadSheet,
+  getLayoffByState,
+  getStates,
 } = require("./spreadsheet");
+require("dotenv");
 
 app.get("/", async (req, res) => {
-  const values = await getSpreadSheet();
+  const values = await getAllLayoff();
 
   res.send(values);
 });
 
-
-
-
-app.get("/:page", async (req, res) => {
-  const values = await getSpreadSheet(req.params.page);
+app.get("/page/:page", async (req, res) => {
+  const values = await getAllLayoff(req.params.page);
 
   res.send(values);
 });
@@ -28,9 +28,17 @@ app.get("/record-count", async (req, res) => {
   res.status(200).json(pages);
 });
 
+// Get Layoff By State
+app.get("/state/:state/:page", async (req, res) => {
+  const { state, page } = req.params;
+  const values = await getLayoffByState(parseInt(page), state);
+  res.status(200).send(values);
+});
 
-
-
-app.listen(port, (req, res) => {
+app.get("/state", async (req, res) => {
+  const states = await getStates();
+  res.status(200).send(states);
+});
+app.listen(port, () => {
   console.log(`Running on Port: ${port}`);
 });
--- backend/spreadsheet.js
+++ backend/spreadsheet.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 const { google, compute_alpha } = require("googleapis");
 
-async function getSpreadSheet(page = 1) {
+const googleSheets = google.sheets({
+  version: "v4",
+  auth: process.env.GOOGLE_API_KEY,
+});
+const spreadsheetId = process.env.SPREADSHEET_ID;
+
+const getSpreadSheet = async (page) => {
+  const response = await googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: `Sheet1!A${(page - 1) * 500 + 1}:Z${page * 500}`,
+  });
+
+  return response.data;
+};
+
+async function getAllLayoff(page = 1) {
   try {
-    const googleSheets = google.sheets({
-      version: "v4",
-      auth: process.env.GOOGLE_API_KEY,
-    });
+    const spreadsheet = await getSpreadSheet(page);
 
-    const spreadsheetId = process.env.SPREADSHEET_ID;
-
-    const response = await googleSheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: `Sheet1!A${(page - 1) * 500 + 1}:Z${page * 500}`,
-    });
-
-    console.log("Sheet1: ", response.data.values[0]);
-
-    const values = response.data.values;
+    const values = spreadsheet.values;
 
     const companies = {};
 
@@ -38,25 +41,65 @@ async function getSpreadSheet(page = 1) {
 
 async function getTotalRecordCountSpreadSheet() {
   try {
-    const googleSheets = google.sheets({
-      version: "v4",
-      auth: process.env.GOOGLE_API_KEY,
-    });
-
-    const spreadsheetId = process.env.SPREADSHEET_ID;
-
     const response = await googleSheets.spreadsheets.get({
       spreadsheetId,
+      ranges: "Sheet1!A:Z",
     });
 
     let totalCount = response.data.sheets[0].properties.gridProperties.rowCount;
 
     const pageSize = 500;
 
-    return totalCount / pageSize;
+    return parseInt(totalCount / pageSize);
   } catch (error) {
     console.log(error);
   }
 }
 
-module.exports = { getSpreadSheet, getTotalRecordCountSpreadSheet };
+async function getLayoffByState(page = 1, state) {
+  try {
+    state = state.toLowerCase(); // Capitalizing values
+
+    const spreadsheet = await getSpreadSheet(page);
+
+    const values = spreadsheet.values;
+    const stateLayoff = [];
+    for (let i = 0; i < values.length; i++) {
+      const record = values[i];
+      if (record[0].toLowerCase() == state) {
+        console.log("California");
+        stateLayoff.push(record);
+      }
+    }
+    return stateLayoff;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function getStates() {
+  const spreadsheet = await googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: "Sheet1!A:Z",
+  });
+
+  const values = spreadsheet.data.values;
+  const states = {};
+
+  for (let i = 1; i < values.length; i++) {
+    const record = values[i];
+    if (record[0] in states) {
+      states[record[0]]++;
+    } else {
+      states[record[0]] = 1;
+    }
+  }
+
+  return states;
+}
+
+module.exports = {
+  getAllLayoff,
+  getTotalRecordCountSpreadSheet,
+  getLayoffByState,
+  getStates,
+};
