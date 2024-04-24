309facd4c72f9fbf4db2b31dd1cbf11a81ee7584
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-04-24 19:16:45 -0400

    create a sample landing page

--- frontend_redux/src/Components/LandingPage.jsx
+++ frontend_redux/src/Components/LandingPage.jsx
@@ -1,9 +1,12 @@
 import './LandingPage.css'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Button from '@mui/material/Button'
+import dash_image from '../assets/dashboard.png'
 
 const LandingPage = () => {
     return (
-        <Parallax pages={3}>
+        
+        <Parallax pages={4}>
             <ParallaxLayer offset={0} speed={0.2} style={{ height: '100vh' }}>
                 <div style={{
                     display: 'flex',
@@ -17,10 +20,12 @@ const LandingPage = () => {
                     backgroundRepeat: 'no-repeat', // Prevents the image from repeating
                 }}>
                     <h1>Welcome to Our Technology</h1>
+                    
+
                 </div>
             </ParallaxLayer>
     
-            <ParallaxLayer offset={1} speed={0.2} style={{ height: '100vh' }}>
+            <ParallaxLayer offset={1} speed={0.2} style={{ height: '100vh' }} factor={2}>
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
@@ -49,6 +54,32 @@ const LandingPage = () => {
                     backgroundRepeat: 'no-repeat',
                 }}>
                     <h1>Join Us</h1>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer offset={3} speed={0.2} style={{ height: '100vh' }}>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '120vh', // Ensure this is also 100vh
+                    color: 'white',
+                    backgroundColor: '#FCF7EC',
+                    // backgroundImage: `url(https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0)`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                }}>
+                    <div style={{ textAlign: 'center' }}>
+                    <h1 style={{ fontSize: '50px', color: '#00543B', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+                        All Layoff information you need in one place
+                    </h1>
+                    <p style={{ maxWidth: '640px', margin: 'auto', color: '#00543B', fontSize: '20px', lineHeight: '1.6' }}>
+                        Layoff and hiring data across industries one click away. Filtering options to provide in-depth analysis.
+                    </p>
+                    
+                    <img src={dash_image} alt="Layoff Metrics Dashboard" style={{ maxWidth: '50%', height: 'auto' }} />
+                    {/* Additional content like buttons can go here */}
+                    </div>
                 </div>
             </ParallaxLayer>
         </Parallax>
