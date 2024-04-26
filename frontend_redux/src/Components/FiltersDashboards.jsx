e1a2a1bb17dd195663aec737b936c9958c3e65e9
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-26 00:22:32 -0400

    dropdown for the reasons of layoff, options: layoff, closure, reduction

--- frontend_redux/src/Components/FiltersDashboards.jsx
+++ frontend_redux/src/Components/FiltersDashboards.jsx
@@ -4,8 +4,9 @@ const FiltersDashboards = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [dropdownOption, setDropdownOption] = useState("");
 
-  function handleDropdown() {
+  function handleDropdown(event) {
     setDropdownOpen(!dropdownOpen);
+    setDropdownOption(event.target.textContent);
   }
 
   async function searchByState(event) {
@@ -16,9 +17,22 @@ const FiltersDashboards = () => {
   return (
     <div className="filters-dashboard">
       <div className="dropdown">
-        <button className="dropdown-btn">
+        <button className="dropdown-btn" onClick={handleDropdown}>
           {dropdownOption ? dropdownOption : "Reason"}
         </button>
+        {dropdownOpen ? (
+          <div className="menu">
+            <div className="menu-item">
+              <button onClick={handleDropdown}>Layoff</button>
+            </div>
+            <div className="menu-item">
+              <button onClick={handleDropdown}>Closure</button>
+            </div>
+            <div className="menu-item">
+              <button onClick={handleDropdown}>Reduction</button>
+            </div>
+          </div>
+        ) : null}
       </div>
       <form className="search-bar" onSubmit={searchByState}>
         <input type="text" placeholder="Enter a state" />
