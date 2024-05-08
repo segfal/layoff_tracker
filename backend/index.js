df06877abb8a9d3034ba0771a5afc78d4b1b2bae
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-05-07 23:12:17 -0400

    authenticate db in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,55 +3,10 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const {
-  getAllLayoff,
-  getTotalRecordCountSpreadSheet,
-  getLayoffByState,
-  getStates,
-  getLayoffByDate,
-  getByLayoffType,
-} = require("./spreadsheet");
-require("dotenv");
-
+const {authenticateDB} = require('./database/db')
+authenticateDB()
 app.use(cors());
 
-app.get("/record-count", async (req, res) => {
-  const pages = await getTotalRecordCountSpreadSheet();
-
-  res.status(200).json(pages);
-});
-app.get("/:page", async (req, res) => {
-  const values = await getAllLayoff(req.params.page);
-
-  res.send(values);
-});
-
-// Get Layoff By State
-app.get("/state/:state/:page", async (req, res) => {
-  let { state, page } = req.params;
-  state = state.replace(/-/g, " ");
-  const values = await getLayoffByState(parseInt(page), state);
-  res.status(200).send(values);
-});
-
-app.get("/state", async (req, res) => {
-  const states = await getStates();
-  res.status(200).send(states);
-});
-
-app.get("/date/:from/:to/:page", async (req, res) => {
-  const { from, to, page } = req.params;
-
-  const layoffbyDate = await getLayoffByDate(page, from, to);
-  res.status(200).send(layoffbyDate);
-});
-
-app.get("/type/:type/:page", async (req, res) => {
-  const { page, type } = req.params;
-  const laidoffByTypes = await getByLayoffType(page, type);
-
-  res.status(200).send(laidoffByTypes);
-});
 
 app.listen(port, () => {
   console.log(`Running on Port: ${port}`);
