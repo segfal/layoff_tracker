c195f069ca84f7ddf87d00421d37059d698de438
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-05-07 23:11:45 -0400

    connect server to the database

---
+++ backend/database/db.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false 
+  }
+);
+
+async function authenticateDB() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+module.exports = { sequelize, authenticateDB };
